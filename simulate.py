a533b08461ade3059da5a378ecd1bede3f0c0adf
Matthew910818 <107916838+Matthew910818@users.noreply.github.com>
2024-06-15 15:22:02 +0800

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -1,30 +1,25 @@
 import requests
 import time
 
-# 初始值设定
 i = 0
 
-# Flask应用程序的地址
-url = 'http://192.168.108.142:5000/data'
+# Flask地址
+url = 'http://172.20.10.4:5000/data'
 
-# 不停地发送POST请求
+# 發出POST請求
 while True:
-    # 模拟数据
     data = {
         'temperature': str(i),
         'humidity': str(i),
         'pm25': str(i)
     }
     
-    # 发送POST请求
     response = requests.post(url, data=data)
     
-    # 打印请求的结果
     print(f"Status Code: {response.status_code}")
     print(f"Response Text: {response.text}")
     
 
     i += 1
     
-    # 等待1秒再发送下一个请求
     time.sleep(1)
